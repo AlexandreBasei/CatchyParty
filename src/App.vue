e4bd6b75d09324faacce5f2ab27d4e44915427d6
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-03-12 21:48:33 +0100

    fix App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <footerApp></footerApp>
 </template>
 
 <script lang="ts">
