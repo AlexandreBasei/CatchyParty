2fd45738f634ca8c5af8f7e3c02f42fbdf77bcf5
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-03-19 19:39:39 +0100

    ADD GeniusFindLyrics component

---
+++ src/components/FindLyrics/GeniusFindLyrics.vue
@@ -0,0 +1,22 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    //Ici les variables utilisées dans le DOM
+    data() {
+        
+    },
+    // Ici tout le code procédural
+    mounted() {
+
+    },
+    //Ici les fonctions (méthodes)
+    methods: {
+
+    }
+})
+</script>
