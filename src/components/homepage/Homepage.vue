111d415094bd8c4fa6193e963231d2123f96282b
florian.bar91 <florian.bar91@gmail.com>
2024-04-24 11:10:05 +0200

    Fix

--- src/components/homepage/Homepage.vue
+++ src/components/homepage/Homepage.vue
@@ -1,14 +1,13 @@
 <template>
-    <headerApp></headerApp>
     <main v-if="homepage === true">
         <section class="personalization"> 
             <form>
-                <input type="text" class="textInput" placeholder="Entre ton nom d'utilisateur" v-model="pseudo" maxlength="15"
+                <input type="text" class="textInput" :placeholder="$t('UTILISATEUR')" v-model="pseudo" maxlength="15"
                     required>
                 <div class="avatar-choice">
-                    <h3 v-if="homepage === true && !roomId">Choisis un avatar</h3>
+                    <h3 v-if="homepage === true && !roomId">{{ $t('AVATAR') }}</h3>
                     <div v-else v-for="room in rooms" :key="room.id">
-                        <h3 v-if="room.id === roomId">Tu es en train de rejoindre la salle de {{ room.players[0].username }}</h3>
+                        <h3 v-if="room.id === roomId">{{ $t('REJOINDRE_SALLE') }} {{ room.players[0].username }}</h3>
                     </div>
                     <div class="avatar-container">
                         <div class="avatar-selected">
@@ -31,159 +30,193 @@
                             @click="selectAvatar('Avatar3')">
                     </div>
                 </div>
-                <button v-if="homepage === true && !roomId" class="submitBtn" @click="handleSubmit()">Jouer !</button>
+                <button v-if="homepage === true && !roomId" class="submitBtn" @click="handleSubmit()">{{ $t('JOUER') }}</button>
                 <div v-else v-for="room in rooms" :key="room.id">
-                    <button v-if="room.id === roomId" @click="joinRoom(room)" class="submitBtn">Rejoindre la partie !</button>
+                    <button v-if="room.id === roomId" @click="joinRoom(room)" class="submitBtn">{{ $t('REJOINDRE_PARTIE') }}</button>
                 </div>
             </form>
         </section>
 
         <section class="tutorial" v-if="homepage === true">
-            <h3> Comment Jouer ? </h3>
+            <h3> {{ $t('COMMENT_JOUER') }} </h3>
             <div id="tutorial" class="tutorial-box">{{ tutorialText }}</div>
         </section>
     </main>
 
     <gameSelect v-if="!homepage" :socket="socket"></gameSelect>
 
-    <div class="footer">
+    <!-- <div class="footer">
         <footerApp></footerApp>
-    </div>
+    </div> -->
 </template>
 
 <script lang="ts">
-    import footerApp from '../Footer/Footer.vue';
-    import headerApp from '../Header/Header.vue';
-    import io from 'socket.io-client';
-    import gameSelect from '../Game-select/Game-select.vue';
-    import { defineComponent, ref } from 'vue';
+import io from 'socket.io-client';
+import gameSelect from '../game-select/Game-select.vue';
+import { defineComponent, ref } from 'vue';
 
-    interface Room {
-        id: string;
-        players: {
-            host: boolean,
-            avatar: string,
-            roomId: string,
-            socketId: string,
-            username: string,
-            idea: boolean,
-            tabAttributed: boolean,
-        }[];
-    }
+interface Room {
+    id: string;
+    players: {
+        host: boolean,
+        avatar: string,
+        roomId: string,
+        socketId: string,
+        username: string,
+        idea: boolean,
+        tabAttributed: boolean,
+        lang: string,
+    }[];
+}
 
-    export default defineComponent({
-        name: 'home_page',
-        components: {
-            footerApp,
-            headerApp,
-            gameSelect,
-        },
+export default defineComponent({
+    name: 'home_page',
+    components: {
+        gameSelect,
 
-        data() {
-            return {
-                homepage: true,
-                rooms: [] as Room[],
-                pseudo: '',
-                tutorialText: '',
-                socket: io('http://localhost:3000'),
-                steps: [
-                    "Step 1 : Do this...",
-                    "Step 2 : Now, this...",
-                    "Step 3 : And this..."
-                ],
-                currentStep: 0,
-                player: {
-                    host: false,
-                    avatar: "Avatar 1",
-                    roomId: "",
-                    username: "",
-                    socketId: "",
-                    idea: false,
-                    tabAttributed: false,
-                },
-                selectedAvatar: "Avatar1",
+    },
+
+    data() {
+        return {
+            homepage: true,
+            lang: '',
+            rooms: [] as Room[],
+            pseudo: '',
+            tutorialText: '',
+            socket: io('http://localhost:3000'),
+            stepsFr: [
+                "Étape 1 : Choisir un pseudo...",
+                "Étape 2 : Choisir un avatar...",
+                "Étape 3 : Et choisissez vos jeux !"
+            ],
+            stepsEn: [
+                "Step 1 : Choose a username...",
+                "Step 2 : Choose a avatar...",
+                "Step 3 : And select your games !"
+            ],
+            currentStep: 0,
+            player: {
+                host: false,
+                avatar: "Avatar 1",
                 roomId: "",
-            }
-        },
-
-        methods: {
-            updRooms() {
-                this.socket.emit('get rooms');
-
-                this.socket.on('list rooms', (rooms: Room[]) => {
-                    this.rooms = rooms;
-                });
+                username: "",
+                socketId: "",
+                idea: false,
+                tabAttributed: false,
             },
-
-            joinRoom(room: Room) {
-                if (this.pseudo) {
-                    this.player.roomId = room.id;
-                    this.player.username = this.pseudo;
-                    this.player.socketId = this.socket.id ?? "";
-                    this.player.avatar = this.selectedAvatar;
-
-                    this.socket.emit('playerData', this.player);
-                    this.homepage = false;
-                    this.roomId = "";
-                }
-            },
-
-            handleSubmit() {
-                if (this.pseudo) {
-                    this.player.host = true;
-                    this.player.username = this.pseudo;
-                    this.player.socketId = this.socket.id ?? "";
-                    this.player.avatar = this.selectedAvatar;
-
-                    this.socket.emit('playerData', this.player);
-
-                    this.homepage = false;
-                }
-            },
-            nextStep() {
-                this.currentStep++;
-                if (this.currentStep < this.steps.length) {
-                    this.tutorialText = this.steps[this.currentStep];
-                    setTimeout(this.nextStep, 5000);
-                } else {
-                    setTimeout(() => {
-                        this.currentStep = 0;
-                        this.tutorialText = this.steps[this.currentStep];
-                        setTimeout(this.nextStep, 5000);
-                    }, 5000);
-                }
-            },
-            selectAvatar(avatarName: string) {
-                localStorage.setItem('selectedAvatar', avatarName);
-                var options = document.querySelector('.avatar-options');
-                // options.style.display = 'none';
-
-                this.selectedAvatar = avatarName;
-                // this.avatarPath = `/img/${this.player.avatar}.png`;
-            },
-
-            reload() {
-                window.location.search = '?room=';
-            },
-        },
-        mounted() {
-
-            if (this.homepage) {
-                setInterval(() => {
-                    this.updRooms();
-                }, 20);
-            }
-
-            const queryString = window.location.search;
-            const urlParams = new URLSearchParams(queryString);
-            const roomId = urlParams.get('room');
-            this.roomId = roomId ?? "";
-
-            this.nextStep();
+            selectedAvatar: "Avatar1",
+            roomId: "",
         }
-    })
+    },
+
+    methods: {
+        updRooms() {
+            this.socket.emit('get rooms');
+
+            this.socket.on('list rooms', (rooms: Room[]) => {
+                this.rooms = rooms;
+            });
+        },
+
+        joinRoom(room: Room) {
+            if (this.pseudo) {
+                this.player.roomId = room.id;
+                this.player.username = this.pseudo;
+                this.player.socketId = this.socket.id ?? "";
+                this.player.avatar = this.selectedAvatar;
+
+                this.socket.emit('playerData', this.player);
+                this.homepage = false;
+                this.roomId = "";
+            }
+        },
+
+        handleSubmit() {
+            if (this.pseudo) {
+                this.player.host = true;
+                this.player.username = this.pseudo;
+                this.player.socketId = this.socket.id ?? "";
+                this.player.avatar = this.selectedAvatar;
+
+                this.socket.emit('playerData', this.player);
+
+                this.homepage = false;
+            }
+        },
+        nextStepFr() {
+            this.currentStep++;
+            if (this.currentStep < this.stepsFr.length) {
+                this.tutorialText = this.stepsFr[this.currentStep];
+                setTimeout(this.nextStepFr, 5000);
+            } else {
+                setTimeout(() => {
+                    this.currentStep = 0;
+                    this.tutorialText = this.stepsFr[this.currentStep];
+                    setTimeout(this.nextStepFr, 5000);
+                }, 5000);
+            }
+        },
+        nextStepEn() {
+            this.currentStep++;
+            if (this.currentStep < this.stepsEn.length) {
+                this.tutorialText = this.stepsEn[this.currentStep];
+                setTimeout(this.nextStepEn, 5000);
+            } else {
+                setTimeout(() => {
+                    this.currentStep = 0;
+                    this.tutorialText = this.stepsEn[this.currentStep];
+                    setTimeout(this.nextStepEn, 5000);
+                }, 5000);
+            }
+        },
+        selectAvatar(avatarName: string) {
+            localStorage.setItem('selectedAvatar', avatarName);
+            var options = document.querySelector('.avatar-options');
+            // options.style.display = 'none';
+
+            this.selectedAvatar = avatarName;
+            // this.avatarPath = `/img/${this.player.avatar}.png`;
+        },
+
+        reload() {
+            window.location.search = '?room=';
+        },
+
+        changeLanguage() {
+            this.$i18n.locale = localStorage.getItem('lang') || 'fr'
+        },
+    },
+    mounted() {
+
+        const getLang = localStorage.getItem('lang');
+        if(getLang){
+            this.lang = getLang;
+        }else{
+            this.lang = 'fr';
+            localStorage.setItem('lang',this.lang);
+        }
+
+        if (this.homepage) {
+            setInterval(() => {
+                this.updRooms();
+            }, 20);
+        }
+
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const roomId = urlParams.get('room');
+        this.roomId = roomId ?? "";
+
+        if(this.lang == 'fr'){
+            this.nextStepFr();
+        }else{
+            this.nextStepEn();
+        }
+        this.changeLanguage();
+    }
+})
 </script>
 
-<style lang="css" scoped>
-    @import url("./homepage.css");
+<style scoped>
+@import url("./homepage.css");
 </style>
