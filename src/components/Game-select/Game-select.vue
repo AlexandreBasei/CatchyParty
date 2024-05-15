e9bd9ac140e55945aa81f8e835eecf15f6662fd7
Alexandre Basei <alexandreacevedobasei@gmail.com>
2024-05-15 16:59:28 +0200

    AL/feature/enchaînement des jeux

--- src/components/Game-select/Game-select.vue
+++ src/components/Game-select/Game-select.vue
@@ -234,7 +234,18 @@ export default defineComponent({
                 this.currentRound++;
                 this.start();
             }
+            this.socket.emit('sendPlayer', this.player);
         });
+
+        this.socket.on('endgame', () => {
+            if (this.currentRound === this.gamesChosen.length) {
+                console.log("Partie terminée");
+            }
+            else {
+                this.currentRound++;
+                this.start();
+            }
+        })
 
         this.socket.on('get gamesChosen', (gamesChosen: []) => {
             this.gamesChosen = gamesChosen;
