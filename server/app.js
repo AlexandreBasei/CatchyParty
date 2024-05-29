f64bbfef0ae1a6ce2029daac15ebfbe6757ae2c8
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-05-29 22:34:50 +0200

    edit app

--- server/app.js
+++ server/app.js
@@ -522,6 +522,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`Serveur en cours d'exécution sur http://0.0.0.0:${PORT}`);
 });
