f8de26d2f552fba53232ecc79521873055ce57d3
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-06-03 22:17:56 +0200

    config render

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const accessToken = 'UDBHDpaFEmGKgTq1nVV05iYgRYYEiB8pRXAlbxHtuKX-XyHeuWVPeg61itr
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "https://catchyparty.onrender.com",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
