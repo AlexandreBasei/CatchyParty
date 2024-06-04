4d7fc3046d5d81945c3cc8d4a0abceb746a3a4d4
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-06-04 18:03:54 +0200

    config render

--- src/components/Homepage/Homepage.vue
+++ src/components/Homepage/Homepage.vue
@@ -80,7 +80,7 @@ export default defineComponent({
             rooms: [] as Room[],
             pseudo: '',
             tutorialText: '',
-            socket: io('0.0.0.0:10000'),
+            socket: io('0.0.0.0:4000'),
             stepsFr: [
                 "Étape 1 : Choisir un pseudo...",
                 "Étape 2 : Choisir un avatar...",
